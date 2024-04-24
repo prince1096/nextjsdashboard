1db09ffe4ecf3c8e96b8eceed13fb0784696531c
prince1096 <princeraj10121996@gmail.com>
2024-04-25 00:52:49 +0530

    navbar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,11 +1,17 @@
 import Sidebar from "../components/ui/sidebar/Sidebar";
 
 import styles from "../components/ui/dashboard/dashboard.module.css";
+import Navbar from "../components/ui/navbar/Navbar";
 
 const DashLayout = ({ children }) => {
   return (
-    <div className={styles.menu}>
-      <Sidebar />
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+      </div>
       {children}
     </div>
   );
