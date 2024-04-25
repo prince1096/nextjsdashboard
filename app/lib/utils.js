d26a9eefd7b3b6475bdf4cdb7c3be32e23a0af79
prince1096 <princeraj10121996@gmail.com>
2024-04-25 22:57:27 +0530

    mongodb

---
+++ app/lib/utils.js
@@ -0,0 +1,11 @@
+export const connectToDB = async () => {
+  const connection = {};
+
+  try {
+    if (connection.isConnected) return;
+    const db = await mongoose.connect(process.env.MONGODB);
+    connection.isConnected = db.connection[0].readyState;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
